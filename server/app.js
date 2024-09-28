53a4d744937665d76ebfc5729fd419e56aab1a93
caio2983 <caioluna2002@gmail.com>
2024-09-28 05:50:09 -0300

    chore: updated vercel domain

--- server/app.js
+++ server/app.js
@@ -15,7 +15,7 @@ const apiKey_apod = process.env.API_KEY_APOD
 console.log("TESTE 2",apiKey_apod)
 
 fastify.register(require('@fastify/cors'), {
-    origin: 'http://localhost:4200', 
+    origin:['http://localhost:4200', 'https://nasa-interface-uiqo.vercel.app'], 
     methods: ['GET', 'POST', 'PUT', 'DELETE'], 
     allowedHeaders: ['Content-Type', 'Authorization'], 
     credentials: true 
